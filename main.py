7226d66ee0b0923d094aa0b1095fdcae721a6324
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-12-08 23:24:20 +0530

    feat: streaming output

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, Response, jsonify
 import g4f
 import sys
 
 app = Flask(__name__)
+
+def generate_response(model, llm, content):
+    response_generator = g4f.ChatCompletion.create(
+        model=model,
+        provider=llm,
+        messages=[{"role": "user", "content": content}],
+        stream=llm.supports_stream
+    )
+
+    for message in response_generator:
+        yield message
 
 @app.route('/chat_completion', methods=['POST'])
 def chat_completion():
@@ -14,23 +25,14 @@ def chat_completion():
     content = data['content']
     pname = data['provider']
 
-    llm = getattr(getattr(getattr(sys.modules[__name__], "g4f"), "Provider"), pname)
+    try:
+        llm = getattr(getattr(getattr(sys.modules[__name__], "g4f"), "Provider"), pname)
+    except AttributeError:
+        return jsonify({"error": f"Invalid provider: {pname}"}), 400
 
     model = "gpt-3.5-turbo"
 
-    if not llm.supports_gpt_35_turbo:
-        model = "gpt-4"
-
-    response = g4f.ChatCompletion.create(
-        model=model,
-        provider=llm,
-        messages=[{"role": "user", "content": content}],
-    )
-
-    if response.strip() == '':
-        raise Exception('Empty result')
-
-    return jsonify({"response": response})
+    return Response(generate_response(model, llm, content), mimetype="text/event-stream")
 
 @app.route('/working_providers', methods=['GET'])
 def working_providers():
