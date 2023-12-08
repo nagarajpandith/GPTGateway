ca5966243158264d3302d9b1c5790c2558b4003b
nagarajpandith <nagaraj.pandith2002@gmail.com>
2023-12-08 22:23:06 +0530

    feat: create flask api

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GPT Flask API
+
+```
+git clone https://github.com/nagarajpandith/gpt-flask.git
+cd gpt-flask
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+python3 main.py
+```
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+from flask import Flask, request, jsonify
 import g4f
 
-response = g4f.ChatCompletion.create(
-    model="gpt-4",
-    messages=[{"role": "user", "content": "Hello"}],
-    stream=True,
-)
+app = Flask(__name__)
 
-for message in response:
-    print(message, flush=True, end='')
+@app.route('/chat_completion', methods=['POST'])
+def chat_completion():
+    data = request.get_json()
+
+    if 'content' not in data:
+        return jsonify({"error": "Missing 'content' in the request"}), 400
+
+    content = data['content']
+
+    response = g4f.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        provider=g4f.Provider.You,
+        messages=[{"role": "user", "content": content}],
+    )
+
+    return jsonify({"response": response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
